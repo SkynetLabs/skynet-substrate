ad7304e13e96f02643dce0f3f1b64c378ad023e5
Marcin S <marcin@bytedude.com>
2022-03-30 16:37:01 -0500

    Update docs

--- Makefile
+++ Makefile
@@ -8,3 +8,9 @@ lint: fmt
 # Run unit tests.
 test:
 	cargo test -- --nocapture $(run)
+
+doc:
+    cargo doc --no-deps
+    rm -rf ./docs
+    echo '<meta http-equiv="refresh" content="0; url=skynet_substrate/">' > target/doc/index.html
+    cp -r target/doc ./docs
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["skynet_substrate"] = {"name":"","files":["crypto.rs","download.rs","encoding.rs","lib.rs","pin.rs","registry.rs","skylink.rs","upload.rs","util.rs"]};
+sourcesIndex["skynet_substrate"] = {"name":"","files":["crypto.rs","download.rs","encoding.rs","lib.rs","pin.rs","registry.rs","request.rs","skylink.rs","upload.rs","util.rs"]};
 createSourceSidebar();
